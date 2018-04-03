c5cee6ed5d06b77287960fa66fcb829863cd6f51
javimgit1224 <36682043+javimgit1224@users.noreply.github.com>
2018-04-03 16:50:05 -0700

    Add files via upload

--- base.cpp
+++ base.cpp
@@ -7,11 +7,13 @@ int main(int argc, char *argv[])
       "org.gtkmm.examples.base");
 
   Gtk::Window window;
-  window.set_default_size(500, 500);
+  window.set_default_size(1280, 800);
   Gtk::Button m_button;
   m_button.set_label("click me");
   m_button.show();
   window.add(m_button);
+  window.set_title("test gtkmm");
+
 	
 
   return app->run(window);
